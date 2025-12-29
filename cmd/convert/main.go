95ccd2b2ca03c6179b023d8281eb5ed8166805f6
TaisukeFujise <tafujise3502@icloud.com>
2025-12-29 21:46:16 +0900

    first commit

--- cmd/convert/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	// "github.com/TaisukeFujise/Image_converter/internal/<package name>"
-)
-
-func main() {
-
-}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+// "github.com/TaisukeFujise/Image_converter/internal/<package name>"
+
+func main() {
+
+}
