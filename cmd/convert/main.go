2058d79052024b239c3182c62efd0de322b4d830
TaisukeFujise <tafujise3502@icloud.com>
2025-12-29 09:52:37 +0900

    Decide Directory structure

---
+++ cmd/convert/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	// "github.com/TaisukeFujise/Image_converter/internal/<package name>"
+)
+
+func main() {
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/TaisukeFujise/Image_converter
 
-go 1.18
+go 1.25.5
