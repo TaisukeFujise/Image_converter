6309d3b26efbc911775f0f3ba73b05370abd7c61
TaisukeFujise <tafujise3502@icloud.com>
2026-01-02 18:15:42 +0900

    Add main logic except for converting logic

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-// "github.com/TaisukeFujise/Image_converter/internal/<package name>"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+var input string
+var output string
+var args []string
+var root string
+
+func init() {
+	flag.StringVar(&input, "i", "jpg", "input image format")
+	flag.StringVar(&output, "o", "png", "output image format")
+}
 
 func main() {
-
+	flag.Parse()
+	args = flag.Args()
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "error: invalud argument\n")
+		os.Exit(1)
+	}
+	root = args[0]
+	err := filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return fmt.Errorf("error: %s: no such file or directory", root)
+			}
+			if info.IsDir() {
+				return nil
+			}
+			if filepath.Ext(path) == "."+input {
+				// convert .input -> .output
+				return nil
+			}
+			return fmt.Errorf("error: %s is not a valid file", path)
+		})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s", err)
+	}
 }
