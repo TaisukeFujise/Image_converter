117d00dd780715c2b16a9be62d6dffe4c7319829
TaisukeFujise <tafujise3502@icloud.com>
2026-01-04 10:26:47 +0900

    Implement image converting logic, and there are go doc task left.

--- imgconv/imgconv.go
+++ imgconv/imgconv.go
@@ -1,7 +1,92 @@
 package imgconv
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
-func Convert(path string, input string, output string) {
-	fmt.Println("hello")
+type Format string
+
+// TODO
+func ParseFormat(raw string) (result Format, err error) {
+	switch strings.ToLower(raw) {
+	case "jpg":
+		result = ".jpg"
+	case "jpeg":
+		result = ".jpg"
+	case "png":
+		result = ".png"
+	case "gif":
+		result = ".gif"
+	default:
+		result = ""
+		err = fmt.Errorf("%s is not a valid format", raw)
+		return
+	}
+	err = nil
+	return
+}
+
+// pathを受け取って、そのファイルの拡張子がinputで指定された拡張子と一致しているかを確認して、boolを返す
+func (f Format) Match(path string) bool {
+	if filepath.Ext(path) != string(f) {
+		return false
+	}
+	return true
+}
+
+func Convert(path string, output Format) error {
+	reader, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	writer, err := os.Create(filepath.Base(path) + string(output))
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+	switch output {
+	case ".jpg":
+		err = convertToJPEG(reader, writer)
+	case ".png":
+		err = convertToPNG(reader, writer)
+	case ".gif":
+		err = convertToGIF(reader, writer)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func convertToJPEG(r io.Reader, w io.Writer) error {
+	img, _, err := image.Decode(r)
+	if err != nil {
+		return err
+	}
+	return jpeg.Encode(w, img, nil)
+}
+
+func convertToPNG(r io.Reader, w io.Writer) error {
+	img, _, err := image.Decode(r)
+	if err != nil {
+		return err
+	}
+	return png.Encode(w, img)
+}
+
+func convertToGIF(r io.Reader, w io.Writer) error {
+	img, _, err := image.Decode(r)
+	if err != nil {
+		return err
+	}
+	return gif.Encode(w, img, nil)
 }
--- main.go
+++ main.go
@@ -11,25 +11,33 @@ import (
 )
 
 var (
-	input  string
-	output string
-	args   []string
-	root   string
+	rawInput  string
+	rawOutput string
+	input     imgconv.Format
+	output    imgconv.Format
+	err       error
+	root      string
 )
 
 func init() {
-	flag.StringVar(&input, "i", "jpg", "input image format")
-	flag.StringVar(&output, "o", "png", "output image format")
+	flag.StringVar(&rawInput, "i", "jpg", "input image format")
+	flag.StringVar(&rawOutput, "o", "png", "output image format")
 }
 
 func main() {
 	flag.Parse()
-	args = flag.Args()
-	if len(args) != 1 {
-		fmt.Fprintf(os.Stderr, "error: invalid argument\n")
+	root = parseArgs(flag.Args())
+	// TODO
+	input, err = imgconv.ParseFormat(rawInput)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		os.Exit(1)
 	}
-	root = args[0]
+	output, err = imgconv.ParseFormat(rawOutput)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
 	err := filepath.Walk(root,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
@@ -38,8 +46,11 @@ func main() {
 			if info.IsDir() {
 				return nil
 			}
-			if filepath.Ext(path) == "."+input {
-				imgconv.Convert(path, input, output)
+			if input.Match(path) {
+				err = imgconv.Convert(path, output)
+				if err != nil {
+					return err
+				}
 				return nil
 			}
 			return fmt.Errorf("%s is not a valid file", path)
@@ -47,4 +58,12 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 	}
+}
+
+func parseArgs(args []string) string {
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "error: invalid argument\n")
+		os.Exit(1)
+	}
+	return args[0]
 }
